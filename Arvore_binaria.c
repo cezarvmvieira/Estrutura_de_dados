0cc5dffe44da49aae02d95bc54e9910262767769
cezar vinicius <cee.zar08@hotmail.com>
2021-06-26 15:55:08 -0300

    Adicionando a funcao EmOrdem_Personalizado, que mostra todos os pais de nós folhas

--- Arvore_binaria.c
+++ Arvore_binaria.c
@@ -10,20 +10,26 @@ void Inserir(No* aux,int x);
 void EmOrdem(No* aux);
 void PreOrdem(No* aux);
 void PosOrdem(No* aux);
+void EmOrdem_Personalizado(No* aux);
 int main(){
-	No* arv=Construir_No(20);
-	Inserir(arv,18);
-	Inserir(arv,25);
-	Inserir(arv,14);
-	Inserir(arv,13);
-	Inserir(arv,5);
-	Inserir(arv,2);
+	No* arv=Construir_No(91);
+	Inserir(arv,40);
+	Inserir(arv,7);
+	Inserir(arv,24);
+	Inserir(arv,52);
+	Inserir(arv,48);
+	Inserir(arv,88);
+	Inserir(arv,68);
+	Inserir(arv,61);
+	Inserir(arv,97);	
 	printf("Em ordem:");
 	EmOrdem(arv);
 	printf("\nPre ordem:");
 	PreOrdem(arv);
 	printf("\nPos ordem:");
 	PosOrdem(arv);
+	printf("\nPersonalizado:");
+	EmOrdem_Personalizado(arv);
 }
 No* Construir_No(int x){
 	No *novo=malloc(sizeof(No));
@@ -68,4 +74,23 @@ void PosOrdem(No* aux){
 		printf("%d ",aux->info);//visita o nós
 	}
 }
-
+void EmOrdem_Personalizado(No* aux){
+	int achou;
+	if(aux!=NULL){//nao ser nó folha
+		achou=0;
+		EmOrdem_Personalizado(aux->esquerda);//percorre esquerda
+		if(!(aux->esquerda==NULL&&aux->direita==NULL)){//não ser nó folha
+			if(aux->esquerda!=NULL)//existe nó filho a esquerda
+				if(aux->esquerda->esquerda==NULL && aux->esquerda->direita==NULL){//o nó filho ser folha
+					printf("%d ",aux->info);
+					achou=1;	
+				}
+			if(aux->direita!=NULL)//existe nó filho a direita
+				if(aux->direita->esquerda==NULL && aux->direita->direita==NULL)//o nó filho ser folha
+					if(!achou)
+						printf("%d ",aux->info);
+		}
+		EmOrdem_Personalizado(aux->direita);//percorre direita
+	}
+			
+}
